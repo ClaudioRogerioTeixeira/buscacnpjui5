84729c8635c5caf92ddd5a40e442ce2d4489ed47
Teixeira <claudioteixeira@outlook.com>
2021-12-30 11:15:28 -0300

    Initialize repository with easy-ui5

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/te/buscacnpjui5/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.te.buscacnpjui5.controller.Main", {});
+});
