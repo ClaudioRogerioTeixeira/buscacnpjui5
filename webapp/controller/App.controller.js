aae4933d1dd8af6a9883f15a11c3511b36196009
Teixeira <claudioteixeira@outlook.com>
2021-12-31 11:53:17 -0300

    Image text field button

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function(
-	Controller,
-	JSONModel
-) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function(Controller, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("buscacnpj.ui5.controller.App", {
@@ -18,6 +16,10 @@ sap.ui.define([
 		onPress: function(oEvent) {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo(`${oEvent}`)
+		},
+
+		consultaCnpj: function(oEvent) {
+			MessageToast.show('Consulta CNPJ');
 		}
 
 	});
