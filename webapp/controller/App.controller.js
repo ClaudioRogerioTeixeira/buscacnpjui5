68e74a546767ad519e754e5c5c33998a1a827cf1
Teixeira <claudioteixeira@outlook.com>
2022-01-05 10:25:21 -0300

    Ajustes Validação

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,12 +7,13 @@ sap.ui.define([
 
 	return Controller.extend("buscacnpj.ui5.controller.App", {
 
-			onInit: function() {
+			onInit: function() {			
 			this.getView().byId("idIconTabBarNoIcons").setVisible(false);
+			this.getView().byId("button").setEnabled(false);
 		},
 
 		consultaCnpj: function(oEvent) {
-			this.getView().byId("idIconTabBarNoIcons").setVisible(true);
+			this.getView().byId("idIconTabBarNoIcons").setVisible(true);					
 			var cnpj = this.getView().byId("inputCnpj").getValue().toString();			
 			var url = 'https://www.receitaws.com.br/v1/cnpj/' + cnpj.replace(/[^0-9]/g, '');
 			var that = this;
@@ -21,11 +22,18 @@ sap.ui.define([
 				url: url,
 				"dataType": "jsonp",
 				success: function (response) {
-					console.log(response);
 					var oModel = new JSONModel(response);
 					that.getView().setModel(oModel, "data");									
-					MessageToast.show("Consulta efetuada com sucesso.");
-					$(".sapMMessageToast").addClass("sapMMessageToastSuccess");
+					
+					if (response.status == "ERROR") {
+						MessageToast.show("CNPJ INVÁLIDO");
+						$(".sapMMessageToast").addClass("sapMMessageToastDanger");
+						that.onValueHelpRequest(oEvent);
+					} else {
+						MessageToast.show("Consulta efetuada com sucesso.");
+						$(".sapMMessageToast").addClass("sapMMessageToastSuccess");
+					}
+
 				},
 				error: function(error) {
 					MessageToast.show("Consulta não efetuada com sucesso.");
@@ -37,13 +45,28 @@ sap.ui.define([
 
 		onValueHelpRequest: function(oEvent) {
 			this.getView().byId("idIconTabBarNoIcons").setVisible(false);
-			this.getView().byId("inputCnpj").setValue("");		
+			this.getView().byId("inputCnpj").setValue("");
+			this.getView().byId("button").setEnabled(false);
+
+			var msgOnInvalidInput = oEvent.getSource().data()["liveChangeMsgOnInvalidInput"];
+			oEvent.getSource().setValueStateText(" ");
 		},	
 
 		onLiveChange: function(oEvent) {
 			var inputValue = oEvent.getParameter('value').trim();
 			inputValue = inputValue.replace(/[^\d]/g, '');
 			inputValue = inputValue.substring(0, 14);
+
+			var msgOnInvalidInput = oEvent.getSource().data()["liveChangeMsgOnInvalidInput"];
+			if(inputValue.length < 14){
+				oEvent.getSource().setValue(inputValue);								
+				oEvent.getSource().setValueState("Warning");
+				oEvent.getSource().setValueStateText(msgOnInvalidInput);
+			} else {				
+			 	oEvent.getSource().setValueStateText("CNPJ Válido");
+				 this.getView().byId("button").setEnabled(true);
+			}
+						
 			oEvent.getSource().setValue(inputValue);
 		},
 
